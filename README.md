f301a31dbc8356fe604d11566ba0d27152c76ed9
Josh <surettej@bu.edu>
2018-01-24 09:31:52 -0500

    Adding in first intellij build files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # EC500_Building_Software
-Repository used for EC500 special topics project-based course on building complex software systems
+
+How to start the EC500_Building_Software application
+---
+
+1. Run `mvn clean install` to build your application
+1. Start application with `java -jar target/vidTag-1.0-SNAPSHOT.jar server config.yml`
+1. To check that your application is running enter url `http://localhost:8080`
+
+Health Check
+---
+
+To see your applications health enter url `http://localhost:8081/healthcheck`
---
+++ src/main/java/com/ec500/EC500_Building_SoftwareApplication.java
@@ -0,0 +1,29 @@
+package com.ec500;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class EC500_Building_SoftwareApplication extends Application<EC500_Building_SoftwareConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new EC500_Building_SoftwareApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "EC500_Building_Software";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<EC500_Building_SoftwareConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final EC500_Building_SoftwareConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
