273d6d6a27bc8a9d895685b22c1670eaeac17d1c
Josh <surettej@bu.edu>
2018-01-29 23:47:55 -0500

    Reorganizing into interface for abstraction

--- src/main/java/com/ec500/resources/TwitterResource.java
+++ src/main/java/com/ec500/resources/TwitterResource.java
@@ -1,44 +1,31 @@
 package com.ec500.resources;
 
-import com.codahale.metrics.annotation.Timed;
-import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
+import com.ec500.twitter.CollectTweets;
+import com.google.inject.Inject;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("/getTweets")
 @Produces(MediaType.APPLICATION_JSON)
 public class TwitterResource {
 
+    private final CollectTweets collectTweets;
+
+    @Inject
+    public TwitterResource(CollectTweets collectTweets) {
+        this.collectTweets = collectTweets;
+    }
+
     @GET
-    @Timed
-    public ResponseList<Status> getTweets() {
+    @Path("/getTweets")
+    ResponseList<Status> retrieveTweets() {
 
-        // Creates a twitter4j configuration
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("")
-                .setOAuthConsumerSecret("")
-                .setOAuthAccessToken("")
-                .setOAuthAccessTokenSecret("");
-
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-
-        // Gathering 20 responses
-        ResponseList<Status> responseList;
-        try {
-            responseList = twitter.getHomeTimeline();
-        }
-        catch (TwitterException te) {
-            return null;
-        }
-
-        return responseList;
+        return collectTweets.getTweets();
 
     }
 
