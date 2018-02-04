e6c09173e5635d802eb680b23acce2109e11dd06
Josh <surettej@bu.edu>
2018-02-03 19:33:10 -0500

    Adding in finalized functions for the Google CV API

--- src/main/java/org/videoMaker/google/GoogleCVResource.java
+++ src/main/java/org/videoMaker/google/GoogleCVResource.java
@@ -1,7 +1,6 @@
 package org.videoMaker.google;
 
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
 
 import javax.imageio.*;
@@ -14,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/google")
@@ -23,24 +23,41 @@ public class GoogleCVResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public List<List<AnnotateImageResponse>> tagImages(List<String> imageAddresses) {
+
+        List<List<AnnotateImageResponse>> annotateImageResponses = new ArrayList<>();
+
+        // Instantiates a client to talk to the Google CV REST API
         try(ImageAnnotatorClient cv = ImageAnnotatorClient.create()) {
 
             for(String url : imageAddresses) {
 
                 try {
+                    List<AnnotateImageResponse> imageResponse;
+
                     byte[] imageByteArray = createByteArrayFromImageURL(url);
                     ByteString imageInMemory = readImageDataIntoMemory(imageByteArray);
+
+                    List<AnnotateImageRequest> annotateImageRequests = new ArrayList<>();
+                    annotateImageRequests.add(
+                            buildAnnotateImageRequest(imageInMemory)
+                    );
+
+                    imageResponse = getLabelDetectionResponse(
+                            annotateImageRequests,
+                            cv);
+
+                    annotateImageResponses.add(imageResponse);
                 }
                 catch (Exception e) {
                     // TODO: Enter something here
                 }
 
             }
-
         }
         catch (Exception e) {
             // TODO: Enter something here
         }
+        return annotateImageResponses;
     }
 
     public byte[] createByteArrayFromImageURL(String url) throws Exception {
@@ -60,6 +77,30 @@ public class GoogleCVResource {
         ByteString memoryImage = ByteString.copyFrom(imageByteArray);
 
         return memoryImage;
+    }
+
+    public AnnotateImageRequest buildAnnotateImageRequest(ByteString imageBytes) {
+        Image image = Image.newBuilder()
+                .setContent(imageBytes)
+                .build();
+        Feature feature = Feature.newBuilder()
+                .setType(Feature.Type.LABEL_DETECTION)
+                .build();
+
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                .addFeatures(feature)
+                .setImage(image)
+                .build();
+
+        return request;
+    }
+
+    public List<AnnotateImageResponse> getLabelDetectionResponse(List<AnnotateImageRequest> annotateImageRequests,
+                                                                 ImageAnnotatorClient imageAnnotatorClient) {
+        BatchAnnotateImagesResponse response = imageAnnotatorClient.batchAnnotateImages(annotateImageRequests);
+        List<AnnotateImageResponse> responses = response.getResponsesList();
+
+        return responses;
     }
 
 }
