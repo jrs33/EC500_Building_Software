da3408230b0313713b39a537acefb0536a2e89c0
Josh <surettej@bu.edu>
2018-02-05 12:33:13 -0500

    Adding in resources for ffmpeg command line call to create video

--- src/main/java/org/videoMaker/ffmpeg/VideoCreatorResource.java
+++ src/main/java/org/videoMaker/ffmpeg/VideoCreatorResource.java
@@ -1,6 +1,5 @@
 package org.videoMaker.ffmpeg;
 
-import org.videoMaker.google.AnnotatedImages;
 import org.videoMaker.twitter.ImageAddresses;
 
 import javax.ws.rs.*;
@@ -16,24 +15,36 @@ import java.util.List;
 @Path("/ffmpeg")
 public class VideoCreatorResource {
 
+    @Path("/saveImages")
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String videoCreator(@QueryParam("outputPath") String imageOutputPath,
-                               @QueryParam("videoOutputPath") String videoOutputPath,
+    public String fileSaver(@QueryParam("outputPath") String imageOutputPath,
                                ImageAddresses imageAddresses) {
         saveImagesToLocalFile(
                 imageAddresses.getUrlList(),
                 imageOutputPath
         );
 
-        return "Video created!";
+        return "Files saved to " + imageOutputPath + "!";
+    }
+
+    @Path("/makeVideo")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String videoCreator(@QueryParam("imagePath") String imagePath,
+                               @QueryParam("videoPath") String videoPath,
+                               @QueryParam("ffmpegPath") String ffmpegPath) {
+        createAndSaveFFMPEGVideo(imagePath, videoPath, ffmpegPath);
+
+        return "Video saved!";
     }
 
     public void saveImagesToLocalFile(List<String> imageAddresses,
                                       String imageOutputPath) {
         Image image = null;
-        Integer integerConcatenate = 1; // used to uniquely identify each saved image
+        Integer integerConcatenate = 0; // used to uniquely identify each saved image
         for (String url : imageAddresses) {
             try (InputStream in = new URL(url).openStream()) {
                 Files.copy(in, Paths.get(imageOutputPath + "image" + integerConcatenate.toString() + ".jpg"));
@@ -41,6 +52,16 @@ public class VideoCreatorResource {
             } catch (Exception e) {
                 System.out.println("Issue with request");
             }
+        }
+    }
+
+    public void createAndSaveFFMPEGVideo(String imagePath,
+                                         String videoPath,
+                                         String ffmpegPath) {
+        try {
+            Runtime.getRuntime().exec("ffmpeg -r 1 -i image%d.png -s 320x240 -aspect 4:3 output.mp4");
+        } catch (Exception e) {
+            System.out.println("One of the paths was incorrect or ffmpeg cant generate video from listed images");
         }
     }
 
