c3ece5bde6d781da6d1c0817c17a777c98ae0494
Josh <surettej@bu.edu>
2018-04-04 11:22:58 -0400

    adding in logging for video creation

--- src/main/java/org/videoMaker/ffmpeg/VideoCreatorResource.java
+++ src/main/java/org/videoMaker/ffmpeg/VideoCreatorResource.java
@@ -12,10 +12,14 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 @Path("/ffmpeg")
@@ -48,12 +52,24 @@ public class VideoCreatorResource implements LoggedResource {
     }
 
     @Path("/makeVideo")
-    @POST
+    @GET
     @Consumes(MediaType.APPLICATION_JSON)
     public void videoCreator(@QueryParam("imagePath") String imagePath,
                                @QueryParam("videoPath") String videoPath,
                                @DefaultValue("jpg") @QueryParam("fileExtension") String fileExtension) {
         createAndSaveFFMPEGVideo(imagePath, videoPath, fileExtension);
+
+        try {
+            java.nio.file.Path path = Paths.get(videoPath + "output.mp4");
+            BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+
+            DBCollection collection = getffmpegCollection();
+            BasicDBObject object = buildObject(attributes.size());
+            log(collection, object);
+        } catch (Exception e) {
+            System.out.println("ERROR: unable to log data properly");
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
@@ -65,33 +81,52 @@ public class VideoCreatorResource implements LoggedResource {
         }
     }
 
-    public BasicDBObject buildObject(List<String> imageAddresses, String format) {
+    private BasicDBObject buildObject(List<String> imageAddresses, String format) {
         String date = DateTime.now().toString();
         BasicDBObject dbObject =
                 new BasicDBObject("_id", date)
                         .append("imageUrls", imageAddresses)
-                        .append("extension", format);
+                        .append("extension", format)
+                        .append("type", "saved");
 
         return dbObject;
     }
 
-    public void saveImagesToLocalFile(List<String> imageAddresses,
+    private BasicDBObject buildObject(long size) {
+        String date = DateTime.now().toString();
+        BasicDBObject object =
+                new BasicDBObject("_id", date)
+                        .append("size", size)
+                        .append("type", "video");
+
+        return object;
+    }
+
+    private void saveImagesToLocalFile(List<String> imageAddresses,
                                       String fileExtension,
                                       String imageOutputPath) {
-        Image image = null;
         Integer integerConcatenate = 0; // used to uniquely identify each saved image
         for (String url : imageAddresses) {
             try (InputStream in = new URL(url).openStream()) {
                 String pathString = imageOutputPath + "image" + integerConcatenate.toString() + "." +fileExtension;
-                Files.copy(in, Paths.get(pathString));
+                OutputStream os = new FileOutputStream(pathString);
+
+                byte[] b = new byte[2048];
+                int length;
+                while((length = in.read(b)) != -1) {
+                    os.write(b, 0, length);
+                }
+
                 integerConcatenate = integerConcatenate + 1;
+                os.close();
+                in.close();
             } catch (Exception e) {
-                System.out.println("Issue with request");
+                System.out.println("Issue with request. Be sure you are adding a '/' on to the end of your path");
             }
         }
     }
 
-    public void createAndSaveFFMPEGVideo(String imagePath,
+    private void createAndSaveFFMPEGVideo(String imagePath,
                                          String videoPath,
                                          String fileExtension) {
         try {
