9d676a8d467ed585ce7a3adff5b842eb4affd81a
Josh <surettej@bu.edu>
2018-04-16 23:57:09 -0400

    adding in a sql initialization file

---
+++ init.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS `videoMaker`;
+CREATE DATABASE `videoMaker`;
+
+USE `videoMaker`;
+
+CREATE TABLE twitter (date DATETIME, num_images INTEGER);
+CREATE TABLE google (date DATETIME, label VARCHAR(20));
+CREATE TABLE ffmpeg (date DATETIME, url VARCHAR(150));
