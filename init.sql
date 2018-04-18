ad7cb21f868704bb088a6fdeb75e2c7386c0d3a0
Josh <surettej@bu.edu>
2018-04-17 20:14:27 -0400

    adding in minor fixes after testing sql logs

--- init.sql
+++ init.sql
@@ -4,5 +4,5 @@ CREATE DATABASE `videoMaker`;
 USE `videoMaker`;
 
 CREATE TABLE twitter (date DATETIME, num_images INTEGER);
-CREATE TABLE google (date DATETIME, label VARCHAR(20));
+CREATE TABLE google (date DATETIME, label VARCHAR(50));
 CREATE TABLE ffmpeg (date DATETIME, url VARCHAR(150));
